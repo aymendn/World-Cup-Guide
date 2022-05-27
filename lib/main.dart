f514a05c0957798070a2b02f23a07704c108b9a7
aymendn <aymendenoub@gmail.com>
2022-05-27 07:48:16 +0100

    dd

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:last_app/page/onboarding_page.dart';
 
-import 'screens/onboarding_screen.dart';
-
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  static final String title = 'Onboarding Example';
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const OnboradingScreen(),
-      routes: const {},
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: OnBoardingPage(),
+      );
 }
