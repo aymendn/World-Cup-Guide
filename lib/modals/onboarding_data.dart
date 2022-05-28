2a86469a5dfce078c59b6e2ff7faeda78332fd38
aymendn <aymendenoub@gmail.com>
2022-05-28 12:35:52 +0100

    ff

--- lib/modals/onboarding_data.dart
+++ lib/modals/onboarding_data.dart
@@ -1,29 +1,21 @@
-
 class OnboardingContents {
   final String title;
   final String image;
-  final String desc;
 
-  OnboardingContents(
-      {required this.title, required this.image, required this.desc});
+  OnboardingContents({required this.title, required this.image});
 }
 
 List<OnboardingContents> contents = [
   OnboardingContents(
-    title: "Track Your work and get the result",
-    image: "assets/images/image1.png",
-    desc: "Remember to keep track of your professional accomplishments.",
+    title: "Welcome To Qatar22",
+    image: "assets/images/onb1.png",
   ),
   OnboardingContents(
-    title: "Stay organized with team",
-    image: "assets/images/image2.png",
-    desc:
-        "But understanding the contributions our colleagues make to our teams and companies.",
+    title: "Buy Ticktets With Ease",
+    image: "assets/images/onb2.png",
   ),
   OnboardingContents(
-    title: "Get notified when work happens",
-    image: "assets/images/image3.png",
-    desc:
-        "Take control of notifications, collaborate live or on your own time.",
+    title: "Book Flights & Hotels From One Place",
+    image: "assets/images/onb3.jpg",
   ),
 ];
